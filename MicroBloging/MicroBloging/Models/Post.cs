f91aa02d3c5701667cc8d658473f7884798c615e
chaimaI <48823958+chaimaI@users.noreply.github.com>
2020-09-09 16:28:52 +0200

    Ajout des classes

---
+++ MicroBloging/MicroBloging/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroBloging.Models
+{
+    public class Post
+    {
+        public string Text { get; set; }
+        public int [] Reaction { get; set; }
+    }
+}
---
+++ MicroBloging/MicroBloging/Models/Utilisateur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroBloging.Models
+{
+    public class Utilisateur
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Libelle { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int[] Utilisateur { get; set; }
+        public int[] Utilisateur { get; set; }
+
+
+    }
+}
