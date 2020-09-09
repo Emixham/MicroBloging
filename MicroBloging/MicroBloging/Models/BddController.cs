e92024603532af8417c50679638b85937b247ca8
Emixham <Maximed.ipod@gmail.com>
2020-09-09 17:41:07 +0200

    Ajout post dans bdd

--- MicroBloging/MicroBloging/Models/BddController.cs
+++ MicroBloging/MicroBloging/Models/BddController.cs
@@ -21,8 +21,33 @@ namespace MicroBloging.Models
         private void InitConnexion()
         {
             // Création de la chaîne de connexion
-            string connectionString = "SERVER=127.0.0.1; DATABASE=mli; UID=root; PASSWORD=";
+            string connectionString = "SERVER=127.0.0.1; DATABASE=microblogging; UID=root; PASSWORD=root";
             this.connection = new MySqlConnection(connectionString);
+        }
+
+        [HttpPost]
+        public void Index(String Message)
+        {
+            this.connection.Open();
+
+            // Création d'une commande SQL en fonction de l'objet connection
+            MySqlCommand cmd = this.connection.CreateCommand();
+
+            // Requête SQL
+            cmd.CommandText = "INSERT INTO post (contenu, dateheurePost) VALUES (@message,@dateHeure)";
+
+            // utilisation de l'objet contact passé en paramètre
+  
+            cmd.Parameters.AddWithValue("@message", Message);
+            DateTime localDate = DateTime.Now;
+            cmd.Parameters.AddWithValue("@dateHeure", localDate);
+
+
+            // Exécution de la commande SQL
+            cmd.ExecuteNonQuery();
+
+            // Fermeture de la connexion
+            this.connection.Close();
         }
     }
 }
