ac1df013006a4029d0df6154cc4da1141cc67463
Emixham <Maximed.ipod@gmail.com>
2020-09-09 15:55:47 +0200

    Ajout lien bdd

---
+++ MicroBloging/MicroBloging/Models/BddController.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicroBloging.Models
+{
+    public class BddController : Controller
+    {
+        private MySqlConnection connection;
+
+        // Constructeur
+        public void Bdd()
+        {
+            this.InitConnexion();
+        }
+
+        // Méthode pour initialiser la connexion
+        private void InitConnexion()
+        {
+            // Création de la chaîne de connexion
+            string connectionString = "SERVER=127.0.0.1; DATABASE=mli; UID=root; PASSWORD=";
+            this.connection = new MySqlConnection(connectionString);
+        }
+    }
+}
